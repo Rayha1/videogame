cfac582ad40e98ab9ab78699adfc5ef9f92e87c4
Rayha1 <103225542+Rayha1@users.noreply.github.com>
2022-04-08 11:46:00 +1200

    Add files via upload

---
+++ videogames_rater.py
@@ -0,0 +1,8 @@
+##
+# videogame_rater.py
+# rates video games
+
+if __name__ == "__main__":
+    
+    videogames = {"MineCraft":5, "Call Of Duty":1, "Angry Birds":4,
+                  "Splatoon 2":5, "Animal Crossing":4}
