865232be43ff63e96b551064552d009378340561
Rayha1 <103225542+Rayha1@users.noreply.github.com>
2022-04-08 11:49:28 +1200

    Add files via upload

--- videogames_rater.py
+++ videogames_rater.py
@@ -2,7 +2,13 @@
 # videogame_rater.py
 # rates video games
 
+def print_dictionary(dictionary):
+    for game, rating in dictionary.items():
+        print("game:", game, "rating:", rating)
+
 if __name__ == "__main__":
     
     videogames = {"MineCraft":5, "Call Of Duty":1, "Angry Birds":4,
                   "Splatoon 2":5, "Animal Crossing":4}
+
+    print(videogames)
